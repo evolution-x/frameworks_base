3d66d24aa016d34bff5e560a2f7a5cd1fd2cb3b1
minaripenguin <minaripenguin@users.noreply.github.com>
2024-06-12 09:03:41 +0800

    SystemUI: Introduce lockscreen media art filter [1/2]
    * Alternative to lockscreen media feature which Google deprecated
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -92,6 +92,10 @@ import com.android.systemui.wallpapers.data.repository.WallpaperRepository;
 import kotlinx.coroutines.CoroutineDispatcher;
 import kotlinx.coroutines.ExperimentalCoroutinesApi;
 
+import com.android.systemui.util.MediaArtUtils;
+
+import com.android.systemui.util.MediaArtUtils;
+
 import java.io.PrintWriter;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -1194,6 +1198,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                         AlarmTimeout.MODE_IGNORE_IF_SCHEDULED);
             });
         }
+        MediaArtUtils.getInstance(mScrimBehind.getContext()).updateMediaArtVisibility();
     }
 
     /**
